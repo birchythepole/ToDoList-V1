2a93f784a0d1f23d94e6c4e1a51ccaf3c2a275ae
Paul <pawelbrzozowski91@gmail.com>
2020-06-25 17:16:58 +0200

    Added EJS module, and created first view template

--- app.js
+++ app.js
@@ -7,16 +7,38 @@ const app = express();
 
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   let today = new Date();
   let currentDay = today.getDay();
-  if (currentDay === 6 || currentDay === 0 ){
-      res.write("<h1>Yey it's the weekend!</h1>")
-      res.write("<h1>Yey it's the weekend!</h1>")
-      res.send();
-  }else{
-      res.sendFile(__dirname +"/index.html");
+  let day = "";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      day = "Error, wrong day code, CODE: "+currentDay;
   }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(port, () => {
